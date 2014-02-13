5ed07a25cbfbf2d79c7e85c4af62379f285c3adc
Tim Popham <popham@uw.edu>
2014-02-13 11:52:55 -0800

    Fix broken imports

--- unittest/dispatcher.py
+++ unittest/dispatcher.py
@@ -1,7 +1,12 @@
-from ..command.base import DescribeModel, InjectResource, ScopeTransition
+from ..command import DescribeModel, InjectResource
 from ..dispatcher import Dispatcher
+from .command import ScopeTransition
 
 def build_dispatcher(behave_config, suite, context):
+    """
+    Construct a `Dispatcher` instance to provide commands for unittest
+    consumption.
+    """
     command_queue = behave_config.command_queue
     return_queue = behave_config.return_queue
     d = Dispatcher(command_queue, return_queue)
